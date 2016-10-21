8a710227b5497e787a92454315f380a2709a6e25
Valentin Duron <17005852@e003m02.istic.univ-rennes1.fr>
2016-10-21 15:29:14 +0200

    add simple windows

--- fr.istic.m1.aco.miniediteur.v1/src/invoker/MiniIHMStub.java
+++ fr.istic.m1.aco.miniediteur.v1/src/invoker/MiniIHMStub.java
@@ -1,5 +1,27 @@
 package invoker;
 
-public class MiniIHMStub {
+import java.awt.event.*;
+import javax.swing.JFrame;
+
+public class MiniIHMStub extends JFrame implements MiniIHM {
+
+	public MiniIHMStub() {
+		super("MiniACOEditor");
+
+		WindowListener l = new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		};
+		addWindowListener(l);
+		setSize(200, 100);
+		setVisible(true);
+
+	}
+	
+	public static void main(String[] args) {
+		JFrame frame = new MiniIHMStub();
+	}
+
 
 }
