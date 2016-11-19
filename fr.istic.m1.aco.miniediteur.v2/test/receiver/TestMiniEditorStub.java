7b9dc5a64793409312206bbce72690e19c8e83b4
Slynus <Slynus@192.168.1.10>
2016-11-19 16:39:58 +0100

    Add JUnit test for recorder (not really good test)

--- fr.istic.m1.aco.miniediteur.v2/test/receiver/TestMiniEditorStub.java
+++ fr.istic.m1.aco.miniediteur.v2/test/receiver/TestMiniEditorStub.java
@@ -80,4 +80,21 @@ public class TestMiniEditorStub {
 		assertTrue(editor.getBuffer().equals(editor.getSelection()));
 	}
 
+	@Test
+	public void testRecording() {
+		System.out.println("----- testEditorRecord -----");
+		rec = new Recorder();
+		editor = new MiniEditorStub(rec);
+		editor.editorInsert(msgTest);
+
+		editor.startRecording();
+		editor.editorInsert(msgTest);
+		editor.stopRecording();
+
+		editor.playRecording();
+		System.out.println(editor);
+
+		assertTrue(editor.getBuffer().equals(msgTest + msgTest));
+	}
+
 }
