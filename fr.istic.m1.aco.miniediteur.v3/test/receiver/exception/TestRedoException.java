29a2a825a468837bcec79aa029e1de59d2007415
Slynus <lustucru86@gmail.com>
2016-11-25 15:28:26 +0100

    add tests

---
+++ fr.istic.m1.aco.miniediteur.v3/test/receiver/exception/TestRedoException.java
@@ -0,0 +1,14 @@
+package receiver.exception;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRedoException {
+
+	@Test(expected = RedoException.class)
+	public void TestRedo() {
+
+	}
+
+}
---
+++ fr.istic.m1.aco.miniediteur.v3/test/receiver/exception/TestUndoException.java
@@ -0,0 +1,15 @@
+package receiver.exception;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestUndoException {
+	
+	@Test(expected=UndoException.class)
+	public void TestUndo(){
+		
+		
+	}
+	
+}
+
